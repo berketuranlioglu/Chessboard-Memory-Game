b6be3f673b2cb0735fc3d435032062a865b78be4
Berke Turanlıoğlu <berke.turanlioglu@gmail.com>
2023-01-24 00:26:01 +0300

    Initial Commit

---
+++ Chessboard Memory Game/Chessboard_Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chessboard_Memory_GameApp.swift
+//  Chessboard Memory Game
+//
+//  Created by Berke Turanlıoğlu on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Chessboard_Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
